8d9313d5b102c3f0f13ebbc0772cc384a8fc63bc
Maccyts <105554191+Maccyts@users.noreply.github.com>
2022-05-22 20:26:02 +0530

    Create tk2.java

---
+++ tk2.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int n=Integer.parseInt(args[1]);
+	    while(n--!=0)
+	    {
+		System.out.print(args[0]+" ");
+	    }
+	}
+}
+
